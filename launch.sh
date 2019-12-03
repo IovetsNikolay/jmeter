581adf49fd3eb5a19e8ba6dc210cba05149618fd
amerousful <amerousful@gmail.com>
2019-12-03 12:02:18 +0200

    [Jmeter-Docker v1.2] xmx 20% -> 70%

--- launch.sh
+++ launch.sh
@@ -4,11 +4,11 @@ set -e
 freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
 s=$(($freeMem/10*8))
 x=$(($freeMem/10*8))
-n=$(($freeMem/10*2))
+n=$(($freeMem/10*7))
 export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 
 echo "START Running Jmeter on `date`"
-echo "Version 1.1"
+echo "Version 1.2"
 echo "JVM_ARGS=${JVM_ARGS}"
 echo "jmeter args=$@"
 
